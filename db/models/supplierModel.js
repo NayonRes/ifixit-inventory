7a32ee595fdd954f864b6b2391d8f94a801b5e25
Masum Billah <masumbillahbar@gmail.com>
2024-11-09 22:19:05 +0600

    supplier model and controller

---
+++ db/models/supplierModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const supplierSchema = mongoose.Schema({
+  supplier_id: {
+    type: String,
+    required: [true, "Please enter supplier id"],
+  },
+  name: {
+    type: String,
+    required: [true, "Please enter supplier name"],
+    trim: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: [true, "Please enter mobile no"],
+    maxLength: [14, "Mobile cannot exceed 14 characters"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    validate: [validator.isEmail, "Please Enter a valid Email"],
+    trim: true,
+    unique: true,
+  },
+  address: {
+    type: String
+  },
+  remarks: {
+    type: String,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  created_by: {
+    type: String,
+    trim: true,
+    default: "Admin",
+  },
+  created_at: { type: Date, default: Date.now },
+  updated_by: {
+    type: String,
+    trim: true,
+    default: "N/A",
+  },
+  updated_at: { type: Date, default: Date.now },
+});
+ 
+const supplierModel = mongoose.model("supplier", supplierSchema);
+
+const saveData = async () => {
+  let totalData = await supplierModel.countDocuments();
+  console.log("totalData 123456", totalData);
+  if (totalData < 1) {
+    const supDoc = new supplierModel({
+      supplier_id: "s100",
+      name: "Primary",
+      mobile: "01645499001",
+      email: "sup@gmail.com",
+    });
+    await supDoc.save();
+  }
+};
+saveData();
+
+module.exports = supplierModel;
