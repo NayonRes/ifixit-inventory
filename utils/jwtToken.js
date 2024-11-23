e4b98c543d958f7b9b0971a7cddbb904dae2878a
nayonres <nayonres@gmail.com>
2024-11-23 13:30:56 +0600

    set cookie fix in jwt token file

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -12,6 +12,7 @@ const sendToken = (user, statusCode, res) => {
     httpOnly: true, // Cookie is accessible only by the web server
     secure: process.env.NODE_ENV === "production", // Cookie sent only over HTTPS in production
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 'none' for cross-site, 'lax' for same-site
+    domain: "https://ifixit-admin-panel.vercel.app", // Allow cookies to be shared across subdomains
   };
 
   console.log("Running in environment:", process.env.NODE_ENV);
