4937afaac38201f8f1f89055ba94270c06180b60
nayonres <nayonres@gmail.com>
2024-11-23 15:27:05 +0600

    fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,11 +7,11 @@ const sendToken = (user, statusCode, res) => {
   // options for cookie
   const options = {
     expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Cookie is accessible only by the web server
     secure: process.env.NODE_ENV === "production" ? true : false, // Cookie sent only over HTTPS in production
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // 'none' for cross-site, 'lax' for same-site
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 'none' for cross-site, 'lax' for same-site
     // domain: "https://ifixit-admin-panel.vercel.app", // Allow cookies to be shared across subdomains
   };
   console.log("----------options--------------", options);
