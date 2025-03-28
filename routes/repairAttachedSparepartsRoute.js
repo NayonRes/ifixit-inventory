b93e2c6ba1149d4447fb0b0bd1a00fa5656877e8
NayonRes <nayonres@gmail.com>
2025-03-28 17:19:44 +0600

    fix

---
+++ routes/repairAttachedSparepartsRoute.js
@@ -0,0 +1,46 @@
+var express = require("express");
+const {
+  getDataWithPagination,
+  getAllStock,
+  getById,
+  createData,
+  updateData,
+  purchaseReturn,
+  stockAdjustment,
+  deleteData,
+} = require("../controller/repairAttachedSparepartsController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+var router = express.Router();
+
+router
+  .route("/")
+  .get(
+    isAuthenticatedUser,
+    authorizeRoles("stock_list"),
+    getDataWithPagination
+  );
+router
+  .route("/stock-skus-details")
+  .get(isAuthenticatedUser, authorizeRoles("stock_list"), getAllStock);
+router
+  .route("/:id")
+  .get(isAuthenticatedUser, authorizeRoles("view_stock_details"), getById);
+router
+  .route("/create")
+  .post(isAuthenticatedUser, authorizeRoles("add_stock"), createData);
+
+router
+  .route("/update/:id")
+  .put(isAuthenticatedUser, authorizeRoles("update_stock"), updateData);
+router
+  .route("/purchase-return")
+  .post(isAuthenticatedUser, authorizeRoles("update_stock"), purchaseReturn);
+router
+  .route("/stock-adjustment")
+  .post(isAuthenticatedUser, authorizeRoles("update_stock"), stockAdjustment);
+// router
+//   .route("/delete/:id")
+//   .delete(isAuthenticatedUser, authorizeRoles("dashboard"), deleteData);
+
+module.exports = router;
