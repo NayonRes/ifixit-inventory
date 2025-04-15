df86bf1ee8946b5e2691d79ae67807605e9d9ba1
Nayon-Newroz <nayon@newroztech.com>
2025-04-15 18:08:38 +0600

    fix

--- routes/branchRoute.js
+++ routes/branchRoute.js
@@ -40,12 +40,18 @@ router
   .route("/create")
   .post(isAuthenticatedUser, authorizeRoles("add_branch"), createData);
 router.route("/public/list").get(getDataWithPagination);
-router.route("/").get(isAuthenticatedUser, authorizeRoles("update_branch"),getDataWithPagination);
+router
+  .route("/")
+  .get(
+    isAuthenticatedUser,
+    authorizeRoles("branch_list"),
+    getDataWithPagination
+  );
 
 router
   .route("/update/:id")
   .put(isAuthenticatedUser, authorizeRoles("update_branch"), updateData);
-router.route("/public/:id").get(getById); 
+router.route("/public/:id").get(getById);
 router
   .route("/:id")
   .get(isAuthenticatedUser, authorizeRoles("view_branch_details"), getById);
